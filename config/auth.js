fbacd66b623d0436285ed1ae3d3f132eaf6d7985
Spencer Knight <spencer.knightusn@gmail.com>
2022-02-11 13:43:19 -0700

    learning complete

--- config/auth.js
+++ config/auth.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const path = require('path');
-const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const router = require('express').Router();
-const { check } = require('express-validator');
-const LocalStrategy = require('passport-local').Strategy;
 const User = require(path.join(__dirname, '../models/user'));
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
@@ -13,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/login/success',
+      callbackURL: 'http://localhost:3000/login/google/success',
     },
     function (accessToken, refreshToken, profile, cb) {
       const { sub, email, name } = profile._json;
@@ -22,7 +19,6 @@ passport.use(
       const update = { fullname: sub, email, username: name };
       const options = { upsert: true, new: true };
       // Find the document
-      console.log(profile._json);
       User.findOneAndUpdate(query, update, options, function (error, result) {
         if (error) {
           cb(error);
@@ -32,26 +28,6 @@ passport.use(
       });
     }
   )
-);
-// [ DEFINE LOCAL STRATEGY ]
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
-      }
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (res) {
-          return done(null, user);
-        } else {
-          return done(null, false, { message: 'Incorrect password' });
-        }
-      });
-    });
-  })
 );
 
 passport.serializeUser(function (user, done) {
@@ -69,7 +45,7 @@ router.get(
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 router.get(
-  'google/success',
+  '/google/success',
   passport.authenticate('google', {
     failureRedirect: '/login',
     successRedirect: '/',
@@ -77,16 +53,9 @@ router.get(
   })
 );
 
-// [ USERNAME/PASSWORD ROUTES ]
-// router.get(
-//   '/username',
-//   (req, res, next) => res.end('username')
-//   // passport.authenticate('google', { scope: ['profile', 'email'] })
-// );
-
-// router.get('/logout', (req, res, next) => {
-//   req.logout();
-//   res.redirect('/');
-// });
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
